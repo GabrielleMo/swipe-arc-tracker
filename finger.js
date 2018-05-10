4430f7eade20685a1423109596adf38b912b62c1
GabrielleMo <16726155+GabrielleMo@users.noreply.github.com>
2018-05-10 14:08:45 -0500

    Add files via upload

---
+++ finger.js
@@ -0,0 +1,248 @@
+/* -- VECTOR script ends here ------------------------------------ */
+
+
+/* --------------------------------------------------------------- */
+/* -- 'myTracker' script based on Mozilla Touch Events tutorial -- */
+/* -- G Moehring 2018 May 07, version: VERYROUGH ----------------- */
+
+'use strict';
+
+let myTracker = (function() {
+
+	let canvas,
+		context,
+		target,
+		targetCoords,
+		feedback;
+	let	TouchesArray = new Vector();
+	let	arcsArchive = new Vector();
+
+
+// -------- TOOLBOX
+// Initializes a touch object for each finger onscreen. Occurs at first contact.
+	let	createNewTouchRecord = function(changedTouchesArray){
+
+		let cta = changedTouchesArray;
+
+		for (let i = cta.length - 1; i >= 0; i--) {
+
+			let cleanX = Math.round(cta[i].clientX);
+			let cleanY = Math.round(cta[i].clientY);
+
+			return {
+
+				id: cta[i].identifier,
+				xCoord: new Vector().push(cleanX),
+				yCoord: new Vector().push(cleanY)
+			}
+		}
+	}
+// Retrieves the id for a given touch from the array (TouchesArray) storing all touch objects.
+	let lookupTAIndex = function(changedTouchIdentifier){
+
+		let ctid = changedTouchIdentifier;
+
+		for (let i = TouchesArray.length - 1; i >= 0; i--) {
+
+			if (ctid == TouchesArray[i].id){
+				return i;
+			}
+			else {
+				console.log('somethings fucked');
+			}
+		}
+	}
+// Retrieves the last xy coords for a given touch object.
+	let getPrevCoords = function(changedTouchIdentifier){
+
+		let index_TA = lookupTAIndex(changedTouchIdentifier);
+
+		return {
+
+			xCoord: TouchesArray[index_TA].xCoord.last(),
+			yCoord: TouchesArray[index_TA].yCoord.last()
+		}
+	}
+// For each finger (touch) changing state (eg. moving) adds latest xy location to
+// the array (TouchesArray) storing all touch objects.
+	let addCoords = function(changedTouchItem){
+
+			let cti = changedTouchItem;
+			let cleanX = Math.round(cti.xCoord);
+			let cleanY = Math.round(cti.yCoord);
+			let index_TA = lookupTAIndex(cti.identifier);
+
+			TouchesArray[index_TA].xCoord.push(cleanX);
+			TouchesArray[index_TA].yCoord.push(cleanY);
+
+	}
+// The fun part. Draws your finger path.
+	let drawStuff = function(changedTouchesArray){
+
+		let cta = changedTouchesArray;
+
+		for (let i = cta.length - 1; i >= 0; i--) {
+
+			let prevCoords = getPrevCoords(cta[i].identifier);
+			let prevX = prevCoords.xCoord;
+			let prevY = prevCoords.yCoord;
+			let currX = Math.round(cta[i].clientX);
+			let currY = Math.round(cta[i].clientY);
+
+			context.beginPath();
+			context.moveTo(prevX, prevY);
+			context.lineTo(currX, currY);
+			context.stroke();
+
+			TouchesArray[i].xCoord.push(currX);
+			TouchesArray[i].yCoord.push(currY);
+		}
+	}
+// At the end or cancellation of a finger (touch), archives the path each finger(s) took!
+// Then removes ended or cancelled touch from the array tracking active touches (TouchesArray)
+	let archivePath = function(changedTouchesArray){
+
+		let cta = changedTouchesArray;
+
+		for (let i = cta.length - 1; i >= 0; i--) {
+
+			let ctid = cta[i].identifier;
+			let index_TA = lookupTAIndex(ctid);
+			let stamp = Date.now();
+
+			TouchesArray[index_TA].id = stamp;
+
+			arcsArchive.push(TouchesArray[index_TA]);
+
+			TouchesArray.splice(i, 1);
+		}
+	}
+// 	Helper function to give user feedback whether they hit or missed the target.
+	let toggleState = function(elemtochange, zA, zB){
+
+		if (elemtochange.getAttribute('data-state') === zA){
+
+			elemtochange.setAttribute('data-state', zB);
+
+		} else {
+
+			elemtochange.setAttribute('data-state', zA);
+		}
+	}
+// Helper function to evaluate if user hit the target.
+	let bullseye = function(recObj){
+
+		let x = recObj.xCoord[0];
+		let y = recObj.yCoord[0];
+
+		if (((x >= targetCoords.x1) && (x <= targetCoords.x2)) &&
+			((y >= targetCoords.y1) && (y <= targetCoords.y2))) {
+
+			toggleState(feedback, 'visible', 'invisible');
+
+		} else {
+
+			return // ??
+		}
+	}
+// Helper function to reformat objects into arrays. (Ultimately want to export as CSV).
+	let arrayify = function(arcsArchive){ // Make recursive?
+
+		for (let i = arcsArchive.length - 1; i >= 0; i--) { //this may get large so may need to cache length.
+
+			var result = Object.keys(arcsArchive[i]).map(function(key){
+				  return [key, arcsArchive[i][key]];
+			});
+		}
+		console.log(result);
+	}
+
+
+// -------- TOUCH START HANDLER
+	let touchStart = function(evt){
+
+		evt.preventDefault();
+
+		let changedTouches = evt.changedTouches;
+		let newRec = createNewTouchRecord(changedTouches);
+
+		bullseye(newRec);
+
+		TouchesArray.push(newRec);
+	}
+
+
+// -------- TOUCH MOVE HANDLER
+	let touchMove = function(evt){
+
+		evt.preventDefault();
+
+		drawStuff(evt.changedTouches);
+	}
+
+
+// -------- TOUCH END HANDLER
+	var touchEnd = function(evt){
+
+		evt.preventDefault();
+
+		drawStuff(evt.changedTouches);
+
+		archivePath(evt.changedTouches);
+
+		//arrayify(arcsArchive); Need to find best format for export to CSV
+	}
+
+
+// -------- TOUCH CANCEL HANDLER (eg finger wanders off canvas)
+	let touchCancel = function(evt){
+
+		let cta = changedTouchesArray;
+
+		for (let i = cta.length - 1; i >= 0; i--) {
+
+			let ctid = cta[i].identifier;
+			let index_TA = lookupTAIndex(ctid);
+			TouchesArray.splice(index_TA, 1);
+		}
+	}
+
+
+// -------- INITIALIZE THE WHOLE ENCHILADA
+	let initStuff = function(){
+		let scrWidth = window.innerWidth;
+		let scrHeight = window.innerHeight;
+
+		// myTracker 'global' vars
+		canvas = document.getElementsByTagName('canvas')[0];
+		context = canvas.getContext('2d');
+		feedback = document.getElementById('locked');
+		target = document.getElementById('target');
+		targetCoords = {
+			x1: target.offsetLeft,
+			x2: target.offsetLeft + target.clientWidth,
+			y1: target.offsetTop,
+			y2: target.offsetTop + target.clientHeight
+		}
+
+		canvas.setAttribute("width", scrWidth);
+		canvas.setAttribute("height", scrHeight);
+		canvas.addEventListener('touchstart',touchStart,false);
+		canvas.addEventListener('touchmove',touchMove,false);
+		canvas.addEventListener('touchend',touchEnd,false);
+		canvas.addEventListener('touchcancel',touchCancel,false);
+	}
+	return {
+		initStuff: initStuff
+	}
+})();
+
+window.addEventListener('load', myTracker.initStuff);
+
+
+
+
+
+
+
+
---
+++ improved-array-class.js
@@ -0,0 +1,803 @@
+/** ****************************************************************************************************
+ *
+ * A much faster and versatile array class.
+ *
+ * File: index.js
+ * @author Julian Jensen <jjdanois@gmail.com> on 27-AUG-2016
+ * @version 0.7.1
+ *******************************************************************************************************/
+"use strict";
+
+/**
+ * @param subject
+ * @param value
+ * @param start
+ * @param end
+ * @return {*}
+ */
+function fill( subject, value, start, end )
+{
+    var length = subject.length,
+        i;
+    if ( start === undefined )
+        start = 0;
+
+    if ( end === undefined )
+        end = length;
+
+    for ( i = start; i < end; i++ )
+        subject[ i ] = value;
+
+    return subject;
+}
+
+/**
+ * @param subject
+ * @param target
+ * @param fromIndex
+ * @return {number}
+ */
+function indexOf( subject, target, fromIndex )
+{
+    var length = subject.length,
+        i      = 0;
+
+    if ( typeof fromIndex === 'number' )
+    {
+        i = fromIndex;
+
+        if ( i < 0 )
+        {
+            i += length;
+            if ( i < 0 ) i = 0;
+        }
+    }
+
+    for ( ; i < length; i++ )
+    {
+        if ( subject[ i ] === target )
+            return i;
+    }
+
+    return -1;
+}
+
+const
+    iterable         = a => !!a && typeof a[ Symbol.iterator ] === 'function',
+    fixArray        = arr => arr instanceof Vector || Array.isArray( arr ) ? arr : iterable( arr ) ? Vector.from( arr ) : arr !== undefined ? new Vector().push( arr ) : new Vector(),
+    LARGE_ARRAY_SIZE = 200;
+
+/** */
+class Vector extends Array
+{
+    /**
+     * @param args
+     */
+    constructor( ...args )
+    {
+        if ( args.length === 1 && Array.isArray( args[ 0 ] ) )
+        {
+            const
+                input  = args[ 0 ],
+                length = input.length;
+            super( length );
+
+            let i = -1;
+
+            while ( ++i < length )
+                this[ i ] = input[ i ];
+        }
+        else if ( args.length === 1 && typeof args[ 0 ] === 'number' )
+            super( args[ 0 ] );
+        else if ( args.length )
+        {
+            const
+                length = args.length;
+            super( length );
+
+            let i = -1;
+
+            while ( ++i < length )
+                this[ i ] = args[ i ];
+        }
+        else
+            super();
+    }
+
+    /**
+     * @param fn
+     * @param thisContext
+     * @return {Vector}
+     */
+    each( fn, thisContext )
+    {
+        const iterator = thisContext !== undefined ? fn.bind( thisContext ) : fn;
+
+        let index  = -1,
+            length = this.length;
+
+        while ( ++index < length )
+            iterator( this[ index ], index, this );
+
+        return this;
+    }
+
+    /**
+     * @param fn
+     * @return {Function}
+     */
+    static each( fn )
+    {
+        return function( arr ) {
+            arr = arr instanceof Vector || Array.isArray( arr ) ? arr : iterable( arr ) ? Array.from( arr ) : [ arr ];
+
+            const
+                length = arr.length;
+
+            let index = -1;
+
+            while ( ++index < length )
+                fn( arr[ index ], index, arr );
+
+            return arr;
+        };
+    }
+
+    /**
+     * @param fn
+     * @param thisContext
+     * @return {Vector}
+     */
+    map( fn, thisContext )
+    {
+        const
+            length   = this.length,
+            result   = new Vector( length ),
+            iterator = thisContext !== undefined ? fn.bind( thisContext ) : fn;
+
+        let i = -1;
+
+        while ( ++i < length )
+            result[ i ] = iterator( this[ i ], i, this );
+
+        return result;
+    }
+
+    /**
+     * @param fn
+     * @return {Function}
+     */
+    static map( fn )
+    {
+        return function( arr ) {
+            arr = fixArray( arr ); // arr instanceof Vector || Array.isArray( arr ) ? arr : iterable( arr ) ? Vector.from( arr ) : arr !== undefined ? new Vector().push( arr ) : new Vector();
+
+            const
+                length = arr.length,
+                result = new Vector( arr.length );
+
+            let index = -1;
+
+            while ( ++index < length )
+                result[ index ] = fn( arr[ index ], index, arr );
+
+            return result;
+        };
+    }
+
+    /**
+     * @param fn
+     * @param initialValue
+     * @param thisContext
+     * @return {*}
+     */
+    reduce( fn, initialValue, thisContext )
+    {
+        const
+            length   = this.length,
+            iterator = thisContext !== undefined ? fn.bind( thisContext ) : fn;
+
+        let i = 0, result;
+
+        if ( initialValue === undefined )
+        {
+            i = 1;
+            result = this[ 0 ];
+        }
+        else
+            result = initialValue;
+
+        for ( ; i < length; i++ )
+            result = iterator( result, this[ i ], i, this );
+
+        return result;
+    }
+
+    /**
+     * @param fn
+     * @return {Function}
+     */
+    static reduce( fn )
+    {
+        return function( arr, initialValue ) {
+            arr = fixArray( arr ); // arr instanceof Vector || Array.isArray( arr ) ? arr : iterable( arr ) ? Vector.from( arr ) : arr !== undefined ? new Vector().push( arr ) : new Vector();
+
+            const
+                length = arr.length;
+
+            let i = 0, result;
+
+            if ( arguments.length === 1 )
+            {
+                i = 1;
+                result = arr[ 0 ];
+            }
+            else
+                result = initialValue;
+
+            for ( ; i < length; i++ )
+                result = fn( result, arr[ i ], i, arr );
+
+            return result;
+        };
+    }
+
+    /**
+     * @param fn
+     * @param initialValue
+     * @param thisContext
+     * @return {*}
+     */
+    reduceRight( fn, initialValue, thisContext )
+    {
+        const
+            length   = this.length,
+            iterator = thisContext !== undefined ? fn.bind( thisContext ) : fn;
+
+        let i = length - 1, result;
+
+        if ( initialValue === undefined )
+            result = i >= 0 ? this[ i-- ] : undefined;
+        else
+            result = initialValue;
+
+        for ( ; i >= 0; i-- )
+            result = iterator( result, this[ i ], i, this );
+
+        return result;
+    }
+
+    /**
+     * @param args
+     * @return {*}
+     */
+    concat( ...args )
+    {
+        const
+            length = args.length,
+            arr    = Vector.from( this );
+
+        let i = -1, index = this.length;
+
+        while ( ++i < length )
+        {
+            let item = args[ i ];
+
+            if ( Array.isArray( item ) )
+            {
+                let childLength = item.length,
+                    j           = -1;
+
+                while ( ++j < childLength )
+                    arr[ index + j ] = item[ j ];
+
+                index += childLength;
+            }
+            else
+                arr[ index++ ] = item;
+        }
+
+        return arr;
+    }
+
+    /**
+     * @param args
+     * @return {Vector}
+     */
+    push( ...args )
+    {
+        const
+            length = args.length;
+
+        let index = this.length,
+            i     = -1;
+
+        while ( ++i < length )
+            this[ index + i ] = args[ i ];
+
+        return this;
+    }
+
+    /**
+     * @return {*}
+     */
+    pop()
+    {
+        const val = this.length > 0 ? this[ this.length - 1 ] : undefined;
+
+        if ( this.length > 0 ) this.length--;
+
+        return val;
+    }
+
+    /**
+     * @param args
+     * @return {Vector}
+     */
+    unshift( ...args )
+    {
+        this.splice( 0, 0, ...args );
+
+        return this;
+    }
+
+    /**
+     * @return {Vector}
+     */
+    clone()
+    {
+        const
+            length = this.length,
+            sliced = new Vector( length );
+
+        let i = -1;
+
+        while ( ++i < length )
+            sliced[ i ] = this[ i ];
+
+        return sliced;
+    }
+
+    /**
+     * @param input
+     * @return {Vector}
+     */
+    static clone( input )
+    {
+        const
+            length = input.length,
+            sliced = new Vector( length );
+
+        let i = -1;
+
+        while ( ++i < length )
+            sliced[ i ] = input[ i ];
+
+        return sliced;
+    }
+
+    /**
+     * @param fn
+     * @param thisContext
+     * @return {boolean}
+     */
+    every( fn, thisContext )
+    {
+        const
+            length   = this.length,
+            iterator = thisContext !== undefined ? fn.bind( thisContext ) : fn;
+
+        let i = -1;
+
+        while ( ++i < length )
+            if ( !iterator( this[ i ], i, this ) ) return false;
+
+        return true;
+    }
+
+    /**
+     * @param fn
+     * @param thisContext
+     * @return {boolean}
+     */
+    some( fn, thisContext )
+    {
+        const
+            length   = this.length,
+            iterator = thisContext !== undefined ? fn.bind( thisContext ) : fn;
+
+        let i = -1;
+
+        while ( ++i < length )
+            if ( iterator( this[ i ], i, this ) ) return true;
+
+        return false;
+    }
+
+    /**
+     * @param slice
+     * @param sliceEnd
+     * @return {Vector}
+     */
+    slice( slice, sliceEnd )
+    {
+        const ret = new Vector();
+
+        let len = this.length;
+
+        if ( len === 0 ) return ret;
+
+        const start = slice < 0 ? Math.max( 0, slice + len ) : slice || 0;
+
+        if ( sliceEnd !== undefined )
+            len = sliceEnd < 0 ? sliceEnd + len : sliceEnd;
+
+        while ( len-- > start )
+            ret[ len - start ] = this[ len ];
+
+        return ret;
+    }
+
+    /**
+     * @param value
+     * @param start
+     * @param end
+     * @param _dir
+     * @return {*}
+     */
+    fill( value, start, end, _dir )
+    {
+        var i,
+            arr    = this,
+            length = arr.length;
+
+        if ( !_dir ) return fill( this, value, start, end );
+
+        if ( start === undefined ) start = 0;
+
+        if ( end === undefined ) end = length;
+
+        for ( i = start; i < end; i++, value += _dir )
+            arr[ i ] = value;
+
+        return this;
+    }
+
+    /**
+     * @param value
+     * @return {boolean}
+     */
+    includes( value )
+    {
+        if ( Array.isArray( value ) )
+        {
+            const _includes = this.includes.bind( this );
+
+            return value.some( _includes );
+        }
+
+        return this.indexOf( value ) !== -1;
+    }
+
+    /**
+     * @param target
+     * @param fromIndex
+     * @return {number}
+     */
+    indexOf( target, fromIndex )
+    {
+        return indexOf( this, target, fromIndex );
+    }
+
+    /**
+     * @param target
+     * @param fromIndex
+     * @return {number}
+     */
+    lastIndexOf( target, fromIndex )
+    {
+        const
+            length = this.length;
+
+        let i = length - 1;
+
+        if ( typeof fromIndex === 'number' )
+        {
+            i = fromIndex;
+            if ( i < 0 ) i += length;
+        }
+
+        for ( ; i >= 0; i-- )
+            if ( this[ i ] === target ) return i;
+
+        return -1;
+    }
+
+    /**
+     * @param fields
+     * @return {Vector}
+     */
+    pluck( ...fields )
+    {
+        const
+            length  = this.length,
+            plucked = new Vector();
+
+        let count = 0, value, i = -1;
+
+        while ( ++i < length )
+        {
+            value = this[ i ];
+
+            if ( typeof value === 'object' && !!value && !Array.isArray( value ) )
+                plucked[ count++ ] = fields.length === 1 ? value[ fields[ 0 ] ] : fields.map( f => value[ f ] );
+        }
+
+        return plucked;
+    }
+
+    /**
+     * @param set
+     * @return {Vector}
+     * @private
+     */
+    _setToArray( set )
+    {
+        let index  = -1,
+            result = new Vector( set.size );
+
+        set.forEach( value => result[ ++index ] = value );
+
+        return result;
+    }
+
+    /**
+     * @param comparator
+     * @return {Vector}
+     */
+    uniq( comparator )
+    {
+        let index = -1;
+
+        const
+            length = this.length,
+            result = new Vector();
+
+        if ( length >= LARGE_ARRAY_SIZE && !comparator )
+            return this._setToArray( new Set( this ) );
+
+        outer:
+            while ( ++index < length )
+            {
+                const
+                    value = this[ index ];
+
+                let seenIndex = result.length;
+
+                while ( seenIndex-- )
+                    if ( comparator ? comparator( result[ seenIndex ], value ) : result[ seenIndex ] === value ) continue outer;
+
+                result[ result.length ] = value;
+            }
+
+        return result;
+    }
+
+    /**
+     * @param deep
+     * @return {*}
+     */
+    flatten( deep = true )
+    {
+        return Vector._flatten( this, deep );
+    }
+
+    /**
+     * @param arr
+     * @param deep
+     * @param result
+     * @return {Vector}
+     * @private
+     */
+    static _flatten( arr, deep = true, result = new Vector() )
+    {
+        const
+            length = arr.length;
+
+        if ( !length ) return result;
+
+        let index = -1;
+
+        while ( ++index < length )
+        {
+            let value = arr[ index ];
+
+            if ( value instanceof Vector || Array.isArray( value ) )
+            {
+                if ( deep )
+                    Vector._flatten( value, deep, result );
+                else
+                    result.push( ...value );
+            }
+            else
+                result[ result.length ] = value;
+        }
+
+        return result;
+    }
+
+    /**
+     * @return {*}
+     */
+    last()
+    {
+        const length = this.length - 1;
+
+        return length === -1 ? undefined : this[ length ];
+    }
+
+    /**
+     * @param arrs
+     * @return {Vector}
+     */
+    union( ...arrs )
+    {
+        return this.concat( ...arrs ).uniq();
+    }
+
+    /**
+     * @param arrs
+     * @return {Vector}
+     */
+    intersection( ...arrs )
+    {
+        arrs[ arrs.length ] = this;
+
+        let shortest = 0;
+
+        Vector.each( ( a, i ) => a.length < arrs[ shortest ].length && ( shortest = i ) )( arrs );
+
+        let index       = -1,
+            resultIndex = 0,
+            main        = arrs[ shortest ];
+
+        const
+            result = new Vector(),
+            length = main.length;
+
+        skip:
+            while ( ++index < length )
+            {
+                let j           = -1,
+                    childLength = arrs.length,
+                    value       = main[ index ];
+
+                while ( ++j < childLength )
+                    if ( j !== shortest && !arrs[ j ].includes( value ) ) continue skip;
+
+                result[ resultIndex++ ] = value;
+            }
+
+        return result;
+    }
+
+    /**
+     * @param arr
+     * @return {boolean}
+     */
+    equals( arr )
+    {
+        const
+            length = this.length;
+
+        if ( length !== arr.length ) return false;
+
+        const
+            self  = this.sort(),
+            other = arr.sort();
+
+        let i = -1;
+
+        while ( ++i < length )
+            if ( self[ i ] !== other[ i ] ) return false;
+
+        return true;
+    }
+
+    /**
+     * @param hint
+     * @return {*}
+     */
+    [ Symbol.toPrimitive ]( hint )
+    {
+        if ( hint === 'number' ) return this.length;
+        else if ( hint === 'string' ) return '[ ' + this.join( ', ' ) + ' ]';
+
+        return !!this.length;
+    }
+
+    /**
+     * @param inPlace
+     * @return {Vector}
+     */
+    compact( inPlace = false )
+    {
+        const
+            result = inPlace ? this : new Vector(),
+            length = this.length;
+
+        let resultIndex = 0,
+            index       = -1;
+
+        while ( ++index < length )
+            if ( this[ index ] ) result[ resultIndex++ ] = this[ index ];
+
+        if ( inPlace ) result.length = resultIndex;
+
+        return result;
+    }
+
+    /**
+     * @param arg
+     * @return {Vector}
+     */
+    static from( arg )
+    {
+        if ( iterable( arg ) )
+            return Vector.clone( Array.from( arg ) );
+        else
+            return new Vector().push( arg );
+    }
+
+    /**
+     * @param arg
+     * @return {boolean}
+     */
+    static array( arg )
+    {
+        return Array.isArray( arg );
+    }
+
+    /**
+     * @param count
+     * @param min
+     * @param max
+     * @param float
+     * @return {Vector}
+     */
+    static random( count, min = 0, max = 1, float = false )
+    {
+        const
+            a      = new Vector( count ),
+            length = count,
+            _min   = min < max ? min : max,
+            _max   = max > min ? max : min,
+            range  = _max - _min + 1;
+
+        let i = -1, val;
+
+        while ( ++i < length )
+        {
+            val = Math.random() * range;
+            if ( !float ) val |= 0;
+            a[ i ] = val + _min;
+        }
+
+        return a;
+    }
+
+    /**
+     */
+    static override()
+    {
+        require( './proto' );
+    }
+
+}
+
+Vector.prototype.forEach = Vector.prototype.each;
+Vector.forEach = Vector.each;
+Vector.prototype.any = Vector.prototype.some;
+Vector.prototype.all = Vector.prototype.every;
+
+//module.exports = Vector;
